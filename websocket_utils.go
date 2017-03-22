cad18dc2062acbddbf4111d08249c8d6f92a48cb
lycoris0731 <aizu.s1230022@gmail.com>
2017-03-22 18:41:12 +0900

    Support environment variable for set WebSocket port number

--- websocket_utils.go
+++ websocket_utils.go
@@ -3,9 +3,20 @@ package slack
 import (
 	"net"
 	"net/url"
+	"os"
 )
 
 var portMapping = map[string]string{"ws": "80", "wss": "443"}
+
+func init() {
+	if port := os.Getenv("WS_PORT"); port != "" {
+		portMapping["ws"] = port
+	}
+
+	if port := os.Getenv("WSS_PORT"); port != "" {
+		portMapping["wss"] = port
+	}
+}
 
 func websocketizeURLPort(orig string) (string, error) {
 	urlObj, err := url.ParseRequestURI(orig)
